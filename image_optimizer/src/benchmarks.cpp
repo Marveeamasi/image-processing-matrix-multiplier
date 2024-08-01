206ce66b6c4deed2c78f49146242690750c92bb4
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-08-01 07:57:55 +0100

    Update benchmarks.cpp

--- image_optimizer/src/benchmarks.cpp
+++ image_optimizer/src/benchmarks.cpp
@@ -3,5 +3,9 @@
 
 void benchmark(int size) {
     Matrix A(size, size), B(size, size), C(size, size);
- 
+
+    auto start = std::chrono::high_resolution_clock::now();
+    C = A * B;
+    auto end = std::chrono::high_resolution_clock::now();
 }
+
