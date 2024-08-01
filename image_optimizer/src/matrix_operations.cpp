79f3bf283adc59816830f2c4ada46aa47b21260f
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-08-01 08:14:33 +0100

    Update matrix_operations.cpp

--- image_optimizer/src/matrix_operations.cpp
+++ image_optimizer/src/matrix_operations.cpp
@@ -1,6 +1,24 @@
 #include "matrix.h"
 #include <immintrin.h> // For SSE instructions
 
+void matrix_multiply_blocked(const double* A, const double* B, double* C, int M, int N, int K, int block_size) {
+    for (int ii = 0; ii < M; ii += block_size) {
+        for (int jj = 0; jj < N; jj += block_size) {
+            for (int kk = 0; kk < K; kk += block_size) {
+                for (int i = ii; i < std::min(ii + block_size, M); ++i) {
+                    for (int j = jj; j < std::min(jj + block_size, N); ++j) {
+                        double sum = 0.0;
+                        for (int k = kk; k < std::min(kk + block_size, K); ++k) {
+                            sum += A[i * K + k] * B[k * N + j];
+                        }
+                        C[i * N + j] += sum;
+                    }
+                }
+            }
+        }
+    }
+}
+
 Matrix::Matrix(int rows, int cols) {
     rows_ = rows;
     cols_ = cols;
@@ -18,19 +36,13 @@ Matrix Matrix::operator*(const Matrix& other) const {
 
     Matrix result(rows_, other.cols_);
 
-    for (int i = 0; i < rows_; ++i) {
-        for (int j = 0; j < other.cols_; ++j) {
-            double sum = 0.0;
-            for (int k = 0; k < cols_; ++k) {
-                sum += (*this)(i, k) * other(k, j);
-            }
-            result(i, j) = sum;
-        }
-    }
+    int block_size = 64; 
+
+    matrix_multiply_blocked(data_.data(), other.data_.data(), result.data_.data(),
+                           rows_, other.cols_, cols_, block_size);
 
     return result;
 }
-
 
 double Matrix::operator()(int i, int j) const {
     return data_[i * cols_ + j];
